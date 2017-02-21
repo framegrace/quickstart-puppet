ad1788bcbe55437dde29337808fd789fe88678ea
Ian Hill <ianhill@amazon.com>
2017-02-21 09:09:07 -0800

    Fixed RemoteAdminCIDR and updated AMI's

--- scripts/iisserver/init.pp
+++ scripts/iisserver/init.pp
@@ -16,11 +16,13 @@ class iisserver {
   }
 
   windowsfeature { 'Web-WebServer':
+    ensure => present,
+    installsubfeatures => true,
     installmanagementtools => true
   }
   
   file {    
     'c:/inetpub/wwwroot/info.aspx': content => '<%@ Page Trace="true"  Language="C#" ContentType="text/html" ResponseEncoding="utf-8" %>',
-	require => windowsfeature['IIS'],
+	require => windowsfeature['Web-WebServer'],
   }
 }
